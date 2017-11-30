282a4eae3f124ec2827f230439a67d592a2bbbfd
xjflyttp <xjflyttp@gmail.com>
2017-11-30 10:52:59 +0800

    fix basePath

--- DuckyAsset.php
+++ DuckyAsset.php
@@ -12,7 +12,6 @@ class DuckyAsset extends AssetBundle
 {
 
     public $sourcePath = '@bower/ducky/lib';
-    public $basePath = '@webroot/assets';
     public $js = ['ducky.js'];
     public $depends = ['yii\web\JqueryAsset'];
 
